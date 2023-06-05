b47214d53e1f3a261d76b1582496316b5e5cd6b0
SpeedoWBT <speedo.thc@gmail.com>
2023-06-05 13:51:44 +0200

    Update lineage_RMX1931.mk

--- lineage_RMX1931.mk
+++ lineage_RMX1931.mk
@@ -7,34 +7,35 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-$(call inherit-product, device/oppo/OP4A89/OP4A89.mk)
+# Inherit from rmx1931 device
+$(call inherit-product, device/realme/RMX1931/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common ProjectBlaze stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+
+# stuffs
+LINEAGE_BUILD_TYPE := UNOFFICIAL
+LINEAGE_MAINTAINER := noSPEEDO
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+FACE_UNLOCK_SUPPORTED := true
+TARGET_HAS_UDFPS := true
+WITH_GAPPS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_OP4A89
-PRODUCT_DEVICE := OP4A89
-PRODUCT_BRAND := OPPO
-PRODUCT_MODEL := PCLM10
-PRODUCT_MANUFACTURER := OPPO
+PRODUCT_NAME := LINEAGE_RMX1931
+PRODUCT_DEVICE := RMX1931
+PRODUCT_BRAND := realme
+PRODUCT_MANUFACTURER := realme
 
-PRODUCT_AAPT_CONFIG := xxhdpi
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-PRODUCT_CHARACTERISTICS := nosdcard
+PRODUCT_SYSTEM_MODEL := RMX1931
+PRODUCT_SYSTEM_NAME := RMX1931
+PRODUCT_SYSTEM_DEVICE := RMX1931
 
-# Device maintainer
-DEVICE_MAINTAINER := art_chen
-
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Build info
-BUILD_FINGERPRINT := "OPPO/PCLM10/OP4A89:10/QKQ1.191021.002/1587105942:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=OP4A89 \
-    PRODUCT_NAME=PCLM10 \
-    PRIVATE_BUILD_DESC="OP4A89-user 10 QKQ1.191021.002 1587105942 release-keys"
+    PRODUCT_NAME="RMX1931"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
